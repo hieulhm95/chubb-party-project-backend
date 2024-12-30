3c12104c8b70944b730bf78d7e03aa024636fb6c
hieulhm95 <hieulhminh@gmail.com>
2024-12-30 14:58:28 +0700

    fix media

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,11 @@
-FROM node:20-alpine
+FROM node:20-bookworm
 
 COPY package.json .
 COPY package-lock.json .
 
 RUN npm install
+
+RUN apt-get update && apt-get install -y ffmpeg && apt-get clean
 
 COPY . .
 
--- src/controllers/generate.controller.ts
+++ src/controllers/generate.controller.ts
@@ -1,10 +1,33 @@
 import { NextFunction, Request, Response } from 'express';
 import * as generateServices from '../services/generate.services';
-import mime from "mime-types";
-import ffmpeg from "fluent-ffmpeg";
-import ffmpegInstaller from "@ffmpeg-installer/ffmpeg";
+import ffmpeg from 'fluent-ffmpeg';
+import fs from 'fs';
+import ffmpegInstaller from '@ffmpeg-installer/ffmpeg';
 
 ffmpeg.setFfmpegPath(ffmpegInstaller.path);
+
+ffmpeg.getAvailableCodecs((err, codecs) => {
+  if (err) {
+    console.error('Error fetching codecs:', err);
+    return;
+  }
+
+  console.log('Available codecs:');
+  console.log(codecs.aac);
+  console.log(codecs.libmp3lame);
+});
+
+// Check available formats
+ffmpeg.getAvailableFormats((err, formats) => {
+  if (err) {
+    console.error('Error fetching formats:', err);
+    return;
+  }
+
+  console.log('Available formats:');
+  console.log(formats.m4a);
+  console.log(formats.mp3);
+});
 
 export async function getResponses(req: Request, res: Response, next: NextFunction) {
   try {
@@ -16,74 +39,77 @@ export async function getResponses(req: Request, res: Response, next: NextFuncti
   }
 }
 
-export async function getFile(req: Request, res: Response){
+export async function getFile(req: Request, res: Response) {
   const mediaId = req.params.mediaId as string;
-  if(!mediaId) {
-    res.status(404).send("Media not found");
+  if (!mediaId) {
+    res.status(404).send('Media not found');
   }
 
   const result = await generateServices.getFile(mediaId);
-  if(result == null) {
-    return res.status(404).send("Media not found");
+  if (result == null) {
+    return res.status(404).send('Media not found');
   }
 
-  res.setHeader("Content-Type", result.mimeType as string);
+  res.setHeader('Content-Type', result.mimeType as string);
 
   result.content.pipe(res);
 }
 
 const MAPPING_FORMAT = {
-  "audio/mpeg": "mp3",
-  "audio/mp4": "m4a",
-  "audio/x-m4a": "m4a",
-  "audio/wav": "wav",
-  "audio/x-wav": "wav",
-  "audio/ogg": "ogg",
-  "audio/x-flac": "flac",
-  "audio/aac": "aac",
-  "audio/webm": "webm"
-}
+  'audio/mpeg': 'mp3',
+  'audio/mp4': 'm4a',
+  'audio/x-m4a': 'm4a',
+  'audio/wav': 'wav',
+  'audio/x-wav': 'wav',
+  'audio/ogg': 'ogg',
+  'audio/x-flac': 'flac',
+  'audio/aac': 'aac',
+  'audio/webm': 'webm',
+};
 
-export async function getResponse(req: Request, res: Response){
+export async function getResponse(req: Request, res: Response) {
   const mediaId = req.params.mediaId as string;
-  if(!mediaId) {
-    res.status(404).send("Media not found");
+  if (!mediaId) {
+    res.status(404).send('Media not found');
   }
 
   const result = await generateServices.getResponse(mediaId);
-  if(result == null) {
-    return res.status(404).send("Media not found");
+  if (result == null) {
+    return res.status(404).send('Media not found');
   }
 
   return res.status(200).json(result);
 }
-
-export async function getFileWithExtension(req: Request, res: Response){
+export async function getFileWithExtension(req: Request, res: Response) {
   let mediaId = req.params.mediaId as string;
-  if(!mediaId) {
-    res.status(404).send("Media not found");
+  if (!mediaId) {
+    res.status(404).send('Media not found');
   }
 
-  const mediaLinkSegment = mediaId.split(".");
+  const mediaLinkSegment = mediaId.split('.');
   mediaLinkSegment.splice(1, mediaLinkSegment.length - 1);
 
-  mediaId = mediaLinkSegment.join(".")
+  mediaId = mediaLinkSegment.join('.');
   const result = await generateServices.getFile(mediaId);
-  if(result == null) {
-    return res.status(404).send("Media not found");
+  if (result == null) {
+    return res.status(404).send('Media not found');
   }
 
-  res.setHeader("Content-Type", "audio/mp3");
-
-  if(result.mimeType == "audio/mp3" || result.mimeType == "audio/mpeg") {
+  if (result.mimeType == 'audio/mp3' || result.mimeType == 'audio/mpeg') {
     result.content.pipe(res);
-  }
-  else {
-    const stream = ffmpeg(result.content);
-    const inputFormat = (MAPPING_FORMAT as any)[result.mimeType as string] as string;
-    if(inputFormat) stream.inputFormat(inputFormat)
-
-    stream.toFormat("mp3")
-    .pipe(res, {end: true})
+  } else {
+    const mp3Writer = fs.createWriteStream(mediaId + '.m4a');
+    result.content.pipe(mp3Writer, { end: true });
+    result.content.once('close', () => {
+      mp3Writer.end();
+      mp3Writer.close();
+      ffmpeg(mediaId + '.m4a')
+        .toFormat('mp3')
+        .audioCodec('libmp3lame')
+        .save(mediaId + '.mp3')
+        .once('end', () => {
+          fs.createReadStream(mediaId + '.mp3').pipe(res);
+        });
+    });
   }
 }

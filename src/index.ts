428b868b454655ee5a9930ddabf34638aea27fae
hieulhm95 <hieulhminh@gmail.com>
2025-07-03 23:10:44 +0700

    add play, pause socket event

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ import userRouter from './routes/user.routes';
 import { MEDIA_DIR } from './configs/configs';
 import path from 'path';
 import fsPromise from 'fs/promises';
-import { initWebSocket } from './services/websocket.service';
+import { getIO, initWebSocket } from './services/websocket.service';
 const cors = require('cors');
 
 const app: Express = express();
@@ -41,6 +41,39 @@ app.get('/', (req: Request, res: Response) => {
 });
 
 app.use('/api/user', userRouter);
+app.post('/api/media/play', (req: Request, res: Response) => {
+  try {
+    if (!req.body.phase) {
+      return res.status(400).json({ message: 'Phase is required' });
+    }
+    const { phase } = req.body;
+    // Emit to all connected clients
+    const io = getIO();
+    io.emit('play', { phase });
+    logger.info(`[Play]: ${phase}`);
+    res.status(200).json({ message: 'Play updated successfully' });
+  } catch (error) {
+    logger.error(`[Play]: ${error}`);
+    res.status(500).json({ message: 'Play updated failed' });
+  }
+});
+
+app.post('/api/media/pause', (req: Request, res: Response) => {
+  try {
+    if (!req.body.phase) {
+      return res.status(400).json({ message: 'Phase is required' });
+    }
+    const { phase } = req.body;
+    // Emit to all connected clients
+    const io = getIO();
+    io.emit('pause', { phase });
+    logger.info(`[Pause]: ${phase}`);
+    res.status(200).json({ message: 'Pause updated successfully' });
+  } catch (error) {
+    logger.error(`[Pause]: ${error}`);
+    res.status(500).json({ message: 'Pause updated failed' });
+  }
+});
 
 server.listen(port, async () => {
   bootstrap();
